c8511a092cdc7bb5087d8c97c0fa65a7d12c44ce
Woon <cliaz531@gmail.com>
2020-05-21 17:13:26 +0900

    ejs, bodyParser, path Added.

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+var path = require('path')
+var bodyParser = require('body-parser')
+//var ejs = require('ejs')
 var express = require('express')
 var app = express()
 
+//default setting--> mean : 'views' folder is template folder
+//__dirname : folder path from 'C:\' to 'dapi'
+app.set('views',path.join(__dirname+'/views'))
+app.set('view engine','ejs') 
+
+
+//app.use() //middleware use
+
+
+
 app.get('/',function(request,response){
-    console.log(request)
-    response.send("<h1 style='color:red'>Hi</h1>")
+    //response.send("<h1 style='color:red'>Hi</h1>")
+    response.render('index')
 } )
 //listen(port,url,backlog,callback)
 app.listen(8080,function(){
